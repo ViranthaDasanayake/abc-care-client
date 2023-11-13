9fbcb5b60d43d35dce0a8df9d6aef89e6d8ba43d
Virantha Dasanayake <virantha@lbfinance.local>
2023-11-14 00:27:42 +0530

    Fixed routing issue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { 
-    path: '', 
-    redirectTo: 'home', 
-    pathMatch: 'prefix' 
-  },
   {
-    path: 'home',
+    path: '',
     loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
   }
 ];
