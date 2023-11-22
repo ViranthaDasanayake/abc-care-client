c8db78cb0d101f501558f865167a78bd1ca664bd
Virantha Dasanayake <virantha@lbfinance.local>
2023-11-22 10:53:16 +0530

    Added final fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
-  }
+const routes: Routes = [{
+  path: '',
+  redirectTo: 'home',
+  pathMatch: 'prefix'
+},
+{
+  path: 'home',
+  loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+}
 ];
 
 @NgModule({
